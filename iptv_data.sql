bfff0f9b8ac757ee8528fea46eb3494dcdc30105
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2020-11-02 21:53:01 +0300

    PHP IPTV Panel
    PHP IPTV Panel

--- iptv_data.sql
+++ iptv_data.sql
@@ -35,14 +35,6 @@ CREATE TABLE `admin_list` (
   `admin_token` varchar(255) COLLATE utf8_turkish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
---
--- Tablo döküm verisi `admin_list`
---
-
-INSERT INTO `admin_list` (`admin_id`, `admin_email`, `admin_usrname`, `admin_passwd`, `admin_token`) VALUES
-(1, 'alicangonullu@yahoo.com', 'alicangonullu', '060323f33140b4a86b53d01d726a45c7584a3a2b', '060323f33140b4a86b53d01d726a45c7584a3a2b');
-
--- --------------------------------------------------------
 
 --
 -- Tablo için tablo yapısı `iptv_config`
@@ -57,15 +49,6 @@ CREATE TABLE `iptv_config` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 --
--- Tablo döküm verisi `iptv_config`
---
-
-INSERT INTO `iptv_config` (`config_id`, `ffmpeg_m3u8cfg`, `ffmpeg_ts`, `twitter_tkn`, `facebook_tkn`) VALUES
-(1, '-listen 1 -vcodec libx264 -profile:v high -level:v 4.1 -crf 23 -preset veryfast -vf \"yadif\" -s 1920x1080 -maxrate 3400k -bufsize 1600k -hls_wrap 8 -acodec libmp3lame -ac 2 -c:a aac -b:a 128k -strict experimental', '-c:v copy -c:a copy -t 00:05:00', '1234', '1234');
-
--- --------------------------------------------------------
-
---
 -- Tablo için tablo yapısı `ip_block`
 --
 
@@ -75,16 +58,6 @@ CREATE TABLE `ip_block` (
   `ban_reason` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
   `ip_block_active` varchar(255) COLLATE utf8_turkish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
-
---
--- Tablo döküm verisi `ip_block`
---
-
-INSERT INTO `ip_block` (`ip_id`, `ip_adress`, `ban_reason`, `ip_block_active`) VALUES
-(1, '178.243.191.248', 'Nobody', '1'),
-(2, '::1', 'Deneme', '0');
-
--- --------------------------------------------------------
 
 --
 -- Tablo için tablo yapısı `private_iptv`
@@ -99,16 +72,6 @@ CREATE TABLE `private_iptv` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 --
--- Tablo döküm verisi `private_iptv`
---
-
-INSERT INTO `private_iptv` (`private_id`, `private_name`, `private_resim`, `private_iptv`, `private_active`) VALUES
-(1, 'BEIN', '', 'http://hd.vizontele.eu:80/live/server35/server3535/89.m3u8', '1'),
-(3, 'Bein', '', 'http://hd.vizontele.eu:80/live/server35/server3535/89.m3u8', '1');
-
--- --------------------------------------------------------
-
---
 -- Tablo için tablo yapısı `public_iptv`
 --
 
@@ -119,15 +82,6 @@ CREATE TABLE `public_iptv` (
   `video_stream` varchar(255) NOT NULL,
   `public_active` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Tablo döküm verisi `public_iptv`
---
-
-INSERT INTO `public_iptv` (`public_id`, `public_name`, `public_tslink`, `video_stream`, `public_active`) VALUES
-(1, '161fd33f67dbfd29138ce3f165d5e5dd', 'http://demo.codesamplez.com/html5/video/sample', '1', '1'),
-(2, '186a157b2992e7daed3677ce8e9fe40f', 'https://streams.livetv.az/turk/showtv_sd/playlist.m3u8 ', '0', '1'),
-(3, 'cc384c68ad503482fb24e6d1e3b512ae', 'http://hd.vizontele.eu:80/live/server35/server3535/89.m3u8', '0', '1');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
