78446c6145c1533ea042f557e6942d7e56dc4bc2
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2020-11-03 15:12:19 +0300

    PHP IPTV Panel
    PHP IPTV Panel

--- conn.php
+++ conn.php
@@ -1,11 +1,18 @@
 <?php
 error_reporting(0);
+if(file_exists("yukle.lock")) {
+} else {
+die("<center><b>PHP IPTV Yüklenemedi / PHP IPTV was not Installed</b>
+<hr></hr>
+<p>yukle.lock dosyasını silip tekrar deneyin</b><br>
+<a href='install.php'>Yükle</a></center>");
+}
 try {
-  $ip = "localhost"; //host
-  $user = "root";  // host id
-  $password = "";  // password local olduğu için varsayılan şifre
-  $dbad = "iptv_data"; // db adı
-$db = new PDO("mysql:host=$ip;dbname=$dbad", "$user", "$password");
+$ip = "localhost"; //host
+$user = "root";  // host id
+$password = "19742008";  // password local olduğu için varsayılan şifre
+$ad = "iptv_data"; // db adı 
+$db = new PDO("mysql:host=$ip;dbname=$ad", "$user", "$password");
 $db->query("SET CHARACTER SET 'utf8'");
 $db->query("SET NAMES 'utf8'");
 } catch ( PDOException $e ){
--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
-yum -y install screen ffmpeg
+sudo yum -y install epel-release php-fpm php-common php-cli nginx mariadb mariadb-server
+sudo systemctl enable php-fpm.service
+sudo systemctl start php-fpm.service
+sudo systemctl status php-fpm.service
+cat /etc/nginx/conf.d/php-fpm.conf
+ls -l /run/php-fpm/www.sock
+cat /etc/nginx/default.d/php.conf
+sudo systemctl restart nginx.service
+sudo yum -y install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo yum -y install --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm 
+sudo yum -y install epel-release screen 
+sudo rpm -ivh https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
+sudo dnf config-manager --enable PowerTools
+sudo yum -y install ffmpeg ffmpeg-devel
