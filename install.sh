7fa2a1287386214153d7934b68e835db020f178b
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2020-11-07 00:23:48 +0300

    PHP IPTV Panel
    PHP IPTV Panel

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
-sudo yum -y install epel-release php-fpm php-common php-cli nginx mariadb mariadb-server
+sudo yum -y install epel-release nginx screen php-fpm php-common php-cli mariadb mariadb-server 
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install perl perl-devel perl-ExtUtils-Embed libxslt libxslt-devel libxml2 libxml2-devel gd gd-devel GeoIP GeoIP-devel
+wget https://hg.nginx.org/pkg-oss/raw-file/default/build_module.sh
+chmod a+x build_module.sh
+./build_module.sh -v 1.18.0 https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git
+ln -s /usr/lib64/nginx/modules /etc/nginx/modules
+useradd -r -d /var/cache/nginx/ -s /sbin/nologin -U nginx
+mkdir -p /var/cache/nginx/
+chown -R nginx:nginx /var/cache/nginx/
+
+echo "" | /etc/nginx/nginx.conf
+
+mkdir -p /usr/share/nginx/html/hls
+chown -R nginx:nginx /mnt/hls
+systemctl restart nginx
+nginx -t
+
 sudo systemctl enable php-fpm.service
 sudo systemctl start php-fpm.service
 sudo systemctl status php-fpm.service
@@ -8,7 +25,8 @@ cat /etc/nginx/default.d/php.conf
 sudo systemctl restart nginx.service
 sudo yum -y install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo yum -y install --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm 
-sudo yum -y install epel-release screen 
 sudo rpm -ivh https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
 sudo dnf config-manager --enable PowerTools
 sudo yum -y install ffmpeg ffmpeg-devel
+
+echo "
