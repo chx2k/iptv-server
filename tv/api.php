0578c54e2f1492e7ac2891783826c0a574d76d27
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2021-03-26 20:43:47 +0300

    PHP IPTV v2.9
    PHP IPTV v2.9

--- tv/api.php
+++ tv/api.php
@@ -1,6 +1,7 @@
 <?php
-include("tvcon.php");
+include("../conn.php");
 $getir = new IPTVClass();
+session_start();
 $getir->logincheck();
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
@@ -24,28 +25,13 @@ $update->execute();
 while($row = $update->fetch()) {
 echo "<script LANGUAGE='JavaScript'>console.log('OK');</script>";
 }
-
-$update = $db->prepare("INSERT INTO ip_logger(ip, browserinf, date) VALUES (:ipz, :browserz, :datez)");
-$update->bindValue(':ipz', strip_tags("".$_SERVER['REMOTE_ADDR']."".$_SERVER["REQUEST_URI"].""));
-$update->bindValue(':browserz', json_encode(getallheaders()));
-$update->bindValue(':datez', date("Y-m-d H:i:s"));
-$update->execute();
-while($row = $update->fetch()) {
-echo "<script LANGUAGE='JavaScript'>console.log('OK');</script>";
-}
-
-if(isset($_POST["get"])) {
-$stmt2 = $db->prepare('SELECT * FROM public_iptv WHERE public_name = :pubid');
-$stmt2->execute(array(':pubid' => $_POST['get']));
+if(isset($_SESSION['login'])) {
+$stmt2 = $db->prepare('SELECT * FROM private_iptv WHERE private_sahip = :pubid');
+$stmt2->execute(array(':pubid' => $_SESSION['login']));
 $results = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 $json = json_encode($results);
 echo $json;
 } else {
-$stmt2 = $db->prepare('SELECT * FROM public_iptv WHERE public_sahip = :pubid');
-$stmt2->execute(array(':pubid' => $_COOKIE['user_id']));
-$results = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-$json = json_encode($results);
-echo $json;
+die("NO");
 }
-
 ?>
